5ccda4e686fd7ba22d34a0cb197ccb43d2de09b5
Luis Alberto Aquino Barrientos <laquino@xetechs.com>
2022-01-24 15:38:59 -0600

    slm modules 12.0

---
+++ slm_budget_reports/models/__init__.py
@@ -0,0 +1,4 @@
+# -*- coding: utf-8 -*-
+
+from . import budget_encryption_mapping, budget_encryption_report, budget_profit_center_report, \
+    budget_business_unit_report, budget_slm_group_total
---
+++ slm_encryption_reports/models/__init__.py
@@ -0,0 +1,4 @@
+# -*- coding: utf-8 -*-
+
+from . import account_encryption_report, encryption_mapping, profit_center_mapping, account_profit_center_report, \
+    business_unit_mapping, account_business_unit_report, slm_group_mapping, account_slm_group_total, account_break_report
