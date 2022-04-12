43e9cc8d36d934da3327b1e7e85a2b00db52aa4f
Shivam Dudhat <info@laxicon.in>
2022-04-12 17:06:46 +0530

    [IMP]imprrove for contratint

--- branch_accounting_report/models/__init__.py
+++ branch_accounting_report/models/__init__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 # Part of BrowseInfo. See LICENSE file for full copyright and licensing details.
-
+from . import account_move
 from . import inherited_account_reports
 from . import inherited_account_financial_html_report_line
 from . import inherited_account_partner_ledger
